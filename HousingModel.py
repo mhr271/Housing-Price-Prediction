5fb3006099c224a32b4ba8f3a4eaab1ff8946e84
mhr271 <humaidirahman271@gmail.com>
2026-07-12 16:08:31 +1000

    Update HousingModel.py

--- HousingModel.py
+++ HousingModel.py
@@ -24,3 +24,27 @@ preprocessor = ColumnTransformer(
 
     ]
 )
+x_train , x_test , y_train ,y_test = train_test_split(x,y,test_size=0.2,random_state=42)
+print(x_train.shape , x_test.shape , y_train.shape , y_test.shape)
+
+lr = Pipeline([
+    ('preprocess',preprocessor),
+    ('model',LinearRegression())
+])
+
+lr.fit(x_train,y_train)
+
+y_lr_train_pred = lr.predict(x_train)
+y_lr_test_pred = lr.predict(x_test)
+
+lr_train_mse = mean_squared_error(y_train, y_lr_train_pred)
+lr_train_r2 = r2_score(y_train, y_lr_train_pred)
+
+lr_test_mse = mean_squared_error(y_test, y_lr_test_pred)
+lr_test_r2 = r2_score(y_test, y_lr_test_pred)
+
+
+lr_results = pd.DataFrame(
+    [["Linear Regression", lr_train_mse, lr_train_r2, lr_test_mse, lr_test_r2]],
+    columns=["Model", "Train MSE", "Train R2", "Test MSE", "Test R2"],
+)
